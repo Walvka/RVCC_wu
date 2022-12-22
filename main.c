a6a24c323a637e0a3968a3ed4cd46bb8cfd7c857
Walvka <2411082614@qq.com>
2022-12-22 09:42:29 +0800

    支持一元运算符，可以执行长的正负号的判断

--- main.c
+++ main.c
@@ -170,6 +170,7 @@ typedef enum {
     ND_SUB, // -
     ND_MUL, // *
     ND_DIV, // /
+    ND_NEG, // 负号-
     ND_NUM, // 整形
 } NodeKind;
 
@@ -188,6 +189,14 @@ static Node *newNode(NodeKind Kind) {
     Nd->Kind = Kind;
     return Nd;
 }
+
+// 新建一个单叉树
+static Node *newUnary(NodeKind Kind, Node *Expr) {
+  Node *Nd = newNode(Kind);
+  Nd->LHS = Expr;
+  return Nd;
+}
+
 
 // 新建一个二叉树节点
 static Node *newBinary(NodeKind Kind, Node *LHS, Node *RHS) {
@@ -230,10 +239,20 @@ static void pop(char *Reg){
 
 // 生成表达式
 static void genExpr(Node *Nd){
-    // 加载数字到a0
-    if(Nd->Kind == ND_NUM){
-        printf("  li a0, %d\n", Nd->Val);
-        return;
+    // 生成各个根节点
+    switch (Nd->Kind) {
+        // 加载数字到a0
+        case ND_NUM:
+            printf("  li a0, %d\n", Nd->Val);
+            return;
+        // 对寄存器取反
+        case ND_NEG:
+            genExpr(Nd->LHS);
+            // neg a0, a0是sub a0, x0, a0的别名, 即a0=0-a0
+            printf("  neg a0, a0\n");
+            return;
+        default:
+            break;
     }
 
     // 递归到最右节点
@@ -248,33 +267,35 @@ static void genExpr(Node *Nd){
     // 生成各个二叉树节点
     switch (Nd->Kind)
     {
-    case ND_ADD: // + a0=a0+a1
-        printf("  add a0, a0, a1\n");
-        break;
+        case ND_ADD: // + a0=a0+a1
+            printf("  add a0, a0, a1\n");
+            break;
 
-    case ND_SUB: // - a0=a0-a1
-        printf("  sub a0, a0, a1\n");
-        break;  
+        case ND_SUB: // - a0=a0-a1
+            printf("  sub a0, a0, a1\n");
+            break;  
 
-    case ND_MUL: // * a0=a0*a1
-        printf("  mul a0, a0, a1\n");
-        break;
+        case ND_MUL: // * a0=a0*a1
+            printf("  mul a0, a0, a1\n");
+            break;
 
-    case ND_DIV: // / a0=a0/a1
-        printf("  div a0, a0, a1\n");
-        break;
+        case ND_DIV: // / a0=a0/a1
+            printf("  div a0, a0, a1\n");
+            break;
 
-    default:
-        break;
+        default:
+            break;
     }
     return;
 }
 
 // expr = mul ("+" mul | "-" mul)*
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
+// unary = ("+" | "-") unary | primary
 // primary = "(" expr ")" | num
 static Node *expr(Token **Rest, Token *Tok );
 static Node *mul(Token **Rest, Token *Tok );
+static Node *unary(Token **Rest, Token *Tok);
 static Node *primary(Token **Rest, Token *Tok );
 
 // 解析加减
@@ -299,28 +320,45 @@ static Node *expr(Token **Rest, Token *Tok ){
 }
 
 // 解析乘除
-// mul = primary ("*" primary | "/" primary)*
+// // mul = unary ("*" unary | "/" unary)*
 static Node *mul(Token **Rest, Token *Tok ){
-    // primary
-    Node *Nd = primary(&Tok, Tok);
+  // unary
+  Node *Nd = unary(&Tok, Tok);
 
-    // ("*" primary | "/" primary)*
+    // ("*" unary | "/" unary)*
     while (true){
-        // "*" primary
+        // "*" unary
         if(equal(Tok, "*")){
-            Nd = newBinary(ND_MUL, Nd, primary(&Tok, Tok->Next));
+            Nd = newBinary(ND_MUL, Nd, unary(&Tok, Tok->Next));
             continue;
         }
 
-        // "/" primary
+        // "/" unary
         if(equal(Tok, "/")){
-            Nd = newBinary(ND_DIV, Nd, primary(&Tok, Tok->Next));
+            Nd = newBinary(ND_DIV, Nd, unary(&Tok, Tok->Next));
             continue;
         }
 
         *Rest = Tok;
         return Nd;
     }
+}
+
+// 解析一元运算
+// unary = ("+" | "-") unary | primary
+static Node *unary(Token **Rest, Token *Tok) {
+  // "+" unary
+  if (equal(Tok, "+")){
+    return unary(Rest, Tok->Next);
+  }
+
+  // "-" unary
+  if (equal(Tok, "-")){
+    return newUnary(ND_NEG, unary(Rest, Tok->Next));
+  }
+
+  // primary
+  return primary(Rest, Tok);
 }
 
 // 解析括号、数字
