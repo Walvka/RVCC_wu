9df67b70154e42b9c34ce7f3631e87b5f6c0a766
walvka <2411082614@qq.com>
2023-01-04 14:35:42 +0800

    [89] 支持goto和标签语句

--- parse.c
+++ parse.c
@@ -46,6 +46,10 @@ static Scope *Scp = &(Scope){};
 // 指向当前正在解析的函数
 static Obj *CurrentFn;
 
+// 当前函数内的goto和标签列表
+static Node *Gotos;
+static Node *Labels;
+
 // program = (typedef | functionDefinition* | global-variable)*
 // functionDefinition = declspec declarator "(" ")" "{" compoundStmt*
 // declspec = ("void" | "_Bool" | "char" | "short" | "int" | "long"
@@ -69,6 +73,8 @@ static Obj *CurrentFn;
 //        | "if" "(" expr ")" stmt ("else" stmt)?
 //        | "for" "(" exprStmt expr? ";" expr? ")" stmt
 //        | "while" "(" expr ")" stmt
+//        | "goto" ident ";"
+//        | ident ":" stmt
 //        | "{" compoundStmt
 //        | exprStmt
 // exprStmt = expr? ";"
@@ -709,6 +715,8 @@ static bool isTypename(Token *Tok){
 //        | "if" "(" expr ")" stmt ("else" stmt)?
 //        | "for" "(" exprStmt expr? ";" expr? ")" stmt
 //        | "while" "(" expr ")" stmt
+//        | "goto" ident ";"
+//        | ident ":" stmt
 //        | "{" compoundStmt
 //        | exprStmt
 static Node *stmt(Token **Rest, Token *Tok){
@@ -793,6 +801,29 @@ static Node *stmt(Token **Rest, Token *Tok){
         Tok = skip(Tok, ")");
         // stmt
         Nd->Then = stmt(Rest, Tok);
+        return Nd;
+    }
+
+    // "goto" ident ";"
+    if (equal(Tok, "goto")){
+        Node *Nd = newNode(ND_GOTO, Tok);
+        Nd->Label = getIdent(Tok->Next);
+        // 将Nd同时存入Gotos，最后用于解析UniqueLabel
+        Nd->GotoNext = Gotos;
+        Gotos = Nd;
+        *Rest = skip(Tok->Next->Next, ";");
+        return Nd;
+    }
+
+    // ident ":" stmt
+    if (Tok->Kind == TK_IDENT && equal(Tok->Next, ":")){
+        Node *Nd = newNode(ND_LABEL, Tok);
+        Nd->Label = strndup(Tok->Loc, Tok->Len);
+        Nd->UniqueLabel = newUniqueName();
+        Nd->LHS = stmt(Rest, Tok->Next->Next);
+        // 将Nd同时存入Labels，最后用于goto解析UniqueLabel
+        Nd->GotoNext = Labels;
+        Labels = Nd;
         return Nd;
     }
 
@@ -1623,6 +1654,27 @@ static void createParamLVars(Type *Param){
     }
 }
 
+// 匹配goto和标签
+// 因为标签可能会出现在goto后面，所以要在解析完函数后再进行goto和标签的解析
+static void resolveGotoLabels(void) {
+    // 遍历使goto对应上label
+    for (Node *X = Gotos; X; X = X->GotoNext){
+        for (Node *Y = Labels; Y; Y = Y->GotoNext){
+            if (!strcmp(X->Label, Y->Label)){
+                X->UniqueLabel = Y->UniqueLabel;
+                break;
+            }
+        }
+
+        if (X->UniqueLabel == NULL){
+            errorTok(X->Tok->Next, "use of undeclared label");
+        }
+    }
+
+    Gotos = NULL;
+    Labels = NULL;
+}
+
 // functionDefinition = declspec declarator "(" ")" "{" compoundStmt*
 static Token *function(Token *Tok, Type *BaseTy, VarAttr *Attr){
     Type *Ty = declarator(&Tok, Tok, BaseTy);
@@ -1655,6 +1707,9 @@ static Token *function(Token *Tok, Type *BaseTy, VarAttr *Attr){
 
     // 结束当前域
     leaveScope();
+    // 处理goto和标签
+    resolveGotoLabels();
+    
     return Tok;
 }
 
