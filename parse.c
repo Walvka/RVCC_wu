89f01a882903475aa1f9db44b4621778baa49b54
walvka <2411082614@qq.com>
2023-01-04 14:37:27 +0800

    解决typedef和标签之间的冲突

--- parse.c
+++ parse.c
@@ -851,7 +851,7 @@ static Node *compoundStmt(Token **Rest, Token *Tok){
     // (declaration | stmt)* "}"
     while (!equal(Tok, "}")){
         // declaration
-        if (isTypename(Tok)){
+        if (isTypename(Tok) && !equal(Tok->Next, ":")){
             VarAttr Attr = {};
             Type *BaseTy = declspec(&Tok, Tok, &Attr);
 
@@ -1709,7 +1709,7 @@ static Token *function(Token *Tok, Type *BaseTy, VarAttr *Attr){
     leaveScope();
     // 处理goto和标签
     resolveGotoLabels();
-    
+
     return Tok;
 }
 
